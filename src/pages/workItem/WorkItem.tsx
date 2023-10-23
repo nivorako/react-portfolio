5db9c85f0b93d1a3291edd6be68a1455bc2797fd
nivorako <nivo.rakoto@yahoo.fr>
2023-10-23 19:08:15 +0200

    set extern link on workitem page

--- src/pages/workItem/WorkItem.tsx
+++ src/pages/workItem/WorkItem.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+//import { Link } from 'react-router-dom';
+
+import Kizomba from "../../assets/kizomba1.jpg";
+
 import "./index.css";
 
 const WorkItem: React.FC = () => {
+
+    const handleShowWork = () => {
+        window.location.href = "/"
+    }
+
     return (
-        <div>
-                work Itemmmmmm
-        </div>
+        <main className='workItem'>
+                <div className='wokItem_title'>
+                    <h2>WEARE 2GETHER</h2>
+                </div>
+                <div className='wokItem_card'>
+                    <a
+                        href="https://weare2gether-7gzj5dx03-nivorako.vercel.app"
+                        target="_blank" // Cela indique au navigateur d'ouvrir le lien dans un nouvel onglet
+                        className=""
+                    >
+                        <div className='workItem_img' onClick={handleShowWork}>
+                            <img src={Kizomba} alt="html skill" width={150} />
+                        </ div>
+                    </a>
+                </div>
+        </main>
     )
 }
 
