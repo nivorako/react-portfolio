45afb91d55c1a9f9f53ac70f3094fe8b52a73873
nivorako <nivo.rakoto@yahoo.fr>
2025-07-12 01:11:36 +0200

    About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,142 +2,171 @@
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
 import devImage from "../assets/devIMG.webp";
+import { motion } from 'framer-motion';
 
+// Styled Components
 const AboutContainer = styled.div`
     min-height: 100vh;
-    padding: 10rem 2rem 2rem;
+    padding: 2rem 2rem;
     background: var(--background);
     color: var(--text);
-    
-    .about-content {
-        max-width: 1200px;
-        margin: 0 auto;
-        min-height: 80vh;
-        padding: 2rem 0;
-        
-        h1 {
-            text-align: center;
-            margin-bottom: 3rem;
-            grid-column: 1 / -1;
-        }
-        
-        .content-wrapper {
-            display: grid;
-            grid-template-columns: 1fr;
-            grid-template-areas: 
-                "image"
-                 "text";
-            gap: 2rem;
-            
-            @media (max-width: 768px) {
-                grid-template-columns: 1fr 1fr;
-                grid-template-areas: 
-                    "image text"
-                align-items: start;
-                gap: 4rem;
-            }
-        }
+`;
+
+const Content = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    min-height: 80vh;
+    padding: 2rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    text-align: center;
+    margin-bottom: 3rem;
+    font-size: 2.5rem;
+    color: var(--text);
+
+    @media (max-width: 768px) {
+        font-size: 2rem;
     }
-    
-    .image-container {
-        grid-area: image;
-        position: relative;
-        img {
-            width: 100%;
-            max-width: 500px;
-            height: auto;
-            border-radius: 8px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            position: relative;
-            top: 0;
-            margin: 0 auto;
-            display: block; 
-        }
-        @media (min-width: 769px) {
-                position: sticky;
-                top: 2rem;
-                
-                img {
-                    margin: 0;
-                }
-            }
-    }
-    
-    .text-container {
-        grid-area: text;
-        opacity: 0;
-        transform: translateY(20px);
-        transition: opacity 0.6s ease-out, transform 0.6s ease-out;
-        
-        .text-content {
-            display: flex;
-            flex-direction: column;
-            gap: 1.5rem;
-        }
-        
-        &.visible {
-            opacity: 1;
-            transform: translateY(0);
-        }
-        
-        @media (max-width: 768px) {
-            opacity: 1;
-            transform: none;
-            transition: none;
-            
-            &.visible {
-                opacity: 1;
-                transform: none;
-            }
-        }
+`;
+
+const ContentWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+
+    @media (max-width: 768px) {
+    flex-direction: column;  
+`;
+
+const Line = styled(motion.div)`
+    position: absolute;
+    left: 0;
+    height: 2px;
+    background: var(--primary);
+    z-index: 1;
+`;
+
+const ImageContainer = styled.div` 
+    flex: 1;
+    min-width: 0;
+    margin: 1.5rem;
+    position: relative;
+    width: 50%;
+    padding: 80px 0;
+
+    @media (max-width: 768px) {
+        width: 100%;
     }
 
-    h1 {
-        font-size: 2.5rem;
-        margin-bottom: 2rem;
-        color: var(--text);
-        @media (max-width: 768px) {
-            font-size: 2rem;
-            text-align: center;
-        }
-    }
+`;
+ 
+const ProfileImage = styled.img`
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    display: block;
 
-    p {
-        font-size: 1.1rem;
-        line-height: 1.6;
-        color: var(--textSecondary);
-        margin-bottom: 1.5rem;
+    @media (min-width: 769px) {
+        max-width: 500px;
+        height: auto;
     }
+`;
+
+const TextContainer = styled.div<{ inView: boolean }>`
+    flex: 1;
+    min-width: 0;
+    min-width: 300px;
+    opacity: ${({ inView }) => (inView ? 1 : 0)};
+    transform: ${({ inView }) => (inView ? 'translateY(0)' : 'translateY(20px)')};
+    transition: opacity 0.6s ease-out, transform 0.6s ease-out;
+    padding: 0 1rem;
+    box-sizing: border-box;
+    @media (max-width: 768px) {
+        width: 100%;
+    }
+`;
+
+const TextContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+`;
+
+const Paragraph = styled.p`
+    font-size: 1.1rem;
+    line-height: 1.6;
+    color: var(--textSecondary); 
+    margin-bottom: 1.5rem;
 `;
 
 
 const About = () => {
     const { ref, inView } = useInView({
+        triggerOnce: true,
         threshold: 0.1,
-        triggerOnce: true
     });
-    
+
     return (
         <AboutContainer>
-            <div className="about-content">
-                <h1>À propos de moi</h1>
-                <div className="content-wrapper">
-                    <div className="image-container">
-                        <img src={devImage} alt="Développeur web" />
-                    </div>
-                    <div 
-                        className={`text-container ${inView ? 'visible' : ''}`}
-                        ref={ref}
-                    >
-                        <div className="text-content">
-                            <p>Je suis un développeur fullstack passionné, animé par la volonté de créer des expériences web modernes, réactives et intuitives. Après avoir suivi une formation complète en développement web, j'ai poursuivi mon apprentissage en autodidacte, m'immergeant dans des projets concrets et explorant en profondeur les nouvelles tendances technologiques.</p>
-                            <p>Ma formation initiale m'a permis d'acquérir des bases solides en HTML, CSS, JavaScript, ainsi qu'en frameworks tels que React, node js et Express. Ce socle technique s'enrichit continuellement grâce à une veille active et une pratique constante, car je crois que la curiosité et l'envie de progresser sont les meilleurs moteurs d'un développeur.</p>
-                            <p>Au fil du temps, j'ai développé une réelle sensibilité pour les problématiques UX/UI, l'optimisation des performances, et l'architecture backend. J'accorde une attention particulière à l'écriture d'un code propre, modulaire et évolutif. Mon objectif est toujours de construire des applications robustes, faciles à maintenir, et centrées sur les besoins de l'utilisateur.</p>
-                            <p>J'aime relever de nouveaux défis techniques et collaborer sur des projets ambitieux. Que ce soit en frontend ou backend, je m'efforce de proposer des solutions pertinentes et innovantes, tout en restant à l'écoute des retours pour progresser en continu.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Content>
+                <Title>À propos de moi</Title>
+                <ContentWrapper>
+                    <ImageContainer>
+                        <Line
+                            style={{ top: 0, width: '0%' }}
+                            animate={{
+                                width: '90%',
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: 'reverse',
+                                ease: 'easeInOut'
+                            }}
+                        />
+                        <ProfileImage src={devImage} alt="Développeur web" />
+                        <Line
+                            style={{ bottom: 0, width: '0%' }}
+                            animate={{
+                                width: '50%',
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                //repeat: Infinity,
+                                //repeatType: 'reverse',
+                                ease: 'easeInOut',
+                                delay: 0.3
+                            }}
+                        />
+                    </ImageContainer>
+                    <TextContainer ref={ref} inView={inView}>
+                        <TextContent>
+                            <Paragraph>Je suis un développeur fullstack passionné, 
+                                animé par la volonté de créer des expériences web modernes, 
+                                réactives et intuitives. Après avoir suivi une formation 
+                                complète en développement web, j'ai poursuivi 
+                                mon apprentissage en autodidacte, m'immergeant dans 
+                                des projets concrets et explorant en profondeur les nouvelles 
+                                tendances technologiques.
+                            </Paragraph>
+                            <Paragraph>Au fil du temps, j'ai développé une réelle sensibilité pour les problématiques UX/UI, l'optimisation des performances, et l'architecture backend. J'accorde une attention particulière à l'écriture d'un code propre, modulaire et évolutif. Mon objectif est toujours de construire des applications robustes, faciles à maintenir, et centrées sur les besoins de l'utilisateur.</Paragraph>
+                            <Paragraph>J'aime relever de nouveaux défis techniques et collaborer sur des projets ambitieux. </Paragraph>
+                        </TextContent>
+                    </TextContainer>
+                </ContentWrapper>
+                <Paragraph>Actuellement en freelance, je serais très heureux 
+                    de discuter avec vous de vos projets ou de belles 
+                    opportunités professionnelles.
+                </Paragraph>
+            </Content>
         </AboutContainer>
     );
 };
