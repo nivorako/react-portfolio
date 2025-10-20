64916fcebd8e901afad4b35f6a5d9de7c4a1ef84
nivorako <nivo.rakoto@yahoo.fr>
2025-10-20 12:26:37 +0200

    update sendEmail

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { sendEmail } from './lib/email.js';
+import { sendEmail } from './lib/email';
 
 export default async function handler(
   request: VercelRequest,
